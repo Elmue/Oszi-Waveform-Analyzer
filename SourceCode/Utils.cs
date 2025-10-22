e90cdad34aa1edccf29d0c3b7735034519b41d03
Elmue <elmue@gmx.de>
2025-10-22 15:18:29 -0300

    Add files via upload

--- SourceCode/Utils.cs
+++ SourceCode/Utils.cs
@@ -409,7 +409,7 @@ namespace OsziWaveformAnalyzer
 
         public delegate bool delInvokeBool();
 
-        public  const  String     APP_VERSION       = "v2.3"; // displayed in Main Window Title
+        public  const  String     APP_VERSION       = "v2.4"; // displayed in Main Window Title
         public  const  int        MIN_VALID_SAMPLES = 100;    // Error if loaded file contains less samples
         public  const  String     ERR_MIN_SAMPLES   = "The minimum amount of samples is 100.";
         public  const  String     NO_SAMPLES_LOADED = "No samples loaded. Use button 'Capture' or select an Input file.";
